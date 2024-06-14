9a9befd7c65151220d5011178ad997f8306bae37
andressbryce <andressbryce@gmail.com>
2024-06-14 13:03:00 -0400

    chore: JSON Exposure

--- Sources/SmartSpectraIosSDK/Classes/Network/Repositories/Processing/Service.Repository.Processing.swift
+++ Sources/SmartSpectraIosSDK/Classes/Network/Repositories/Processing/Service.Repository.Processing.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Benyamin Mokhtarpour on 6/26/23.
 //
@@ -14,14 +14,14 @@ import Foundation
 
 extension Service.Repository {
     class Processing: PreSignedUrlsProtocol {
-        
+
         /**
          The network manager used for making network requests.
          */
         let service: Service.Network.Manager = .init()
         /**
          Generates pre-signed URLs for file uploads.
-         
+
          - Parameters:
             - data: The request data containing necessary information for generating URLs.
             - completion: The completion handler called when the generation is complete.
@@ -34,7 +34,7 @@ extension Service.Repository {
                   let url = URL(string: Service.Routes.Base.URL.rawValue + Service.Routes.URLs.UploadUrls.rawValue) else {
                 return
             }
-            
+
             do {
                 // Encode the request data
                 let encoder = JSONEncoder()
@@ -44,7 +44,7 @@ extension Service.Repository {
                     switch result {
                     case .success(let responseData):
                         do {
-                            
+
                             let decoder = JSONDecoder()
                             let response = try decoder.decode(Model.Response.PreSignedUrls.self, from: responseData)
                             completion(.success(response))
@@ -60,12 +60,12 @@ extension Service.Repository {
             }
 
         }
-        
-        
+
+
         func multiPartUpload(url: String, fileData: Data ,withCompletion completion: @escaping (Result<String, Error>) -> Void) {
 
             guard let url = URL(string: url) else { return  }
-            
+
             service.putData(from: url, body: fileData) { result in
                 switch result {
                 case .success(let etag):
@@ -76,7 +76,7 @@ extension Service.Repository {
                 }
             }
         }
-        
+
         func retrieveData(body: Model.Request.RetrieveData, withCompletion completion: @escaping (Result<Model.Response.ProcessedData?, Error>) -> Void) {
 
             guard let _ = URL(string: Service.Routes.Base.URL.rawValue),
@@ -95,14 +95,14 @@ extension Service.Repository {
                 service.postData(from: url, body: data) { [weak self] result in
                     guard let self = self else {return}
                     print("POST DATA RESULT", result)
-                    
+
                     switch result {
                     case .success(let responseData):
                         guard !responseData.isEmpty else {
                             completion(.success(nil))
                             return
                         }
-                        
+
                         do {
                             if let json = try JSONSerialization.jsonObject(with: responseData, options: []) as? [String: Any] {
                                 DispatchQueue.main.async {
@@ -113,11 +113,12 @@ extension Service.Repository {
                                 let breathingRates = breath?["rr"] as? [String: [String: Any]]
                                 let pulse = json["pulse"] as? [String: [String: Any]]
                                 let pulseRates = pulse?["hr"] as? [String: [String: Any]]
+                                let pressure = json["pressure"] as? [String: [String: Any]]
 
                                 let uploadDate = json["upload_date"] as? String
-                            
+
                                 let arrayPulseRatesValues = (pulseRates?.values.map { $0["value"] as? Double }.compactMap { $0 })
-                                
+
                                 if let pulsePleth = pulse?["hr_trace"] as? [String: [String: Double]] {
                                     // The data might not be in increasing time order so sort it on time
                                     let timeHrPairs = pulsePleth.compactMap { key, value -> (time: Double, value: Double)? in
@@ -130,7 +131,21 @@ extension Service.Repository {
                                         SharedDataManager.shared.pulsePleth = timeHrPairs
                                     }
                                 }
-                                
+
+                                if let hrWithConfidence = pulse?["hr"] as? [String: [String: Double]] {
+                                    // The data might not be in increasing time order so sort it on time
+                                    let timeHrWithConfidencePairs = hrWithConfidence.compactMap { key, value -> (time: Double, valueWithConfidence: (value: Double, confidence: Double))? in
+                                        if let time = Double(key), let value = (value["value"], value["confidence"]) as? (Double, Double) {
+                                            return (time, value)
+                                        }
+                                        return nil  // Skip invalid entries rather than substituting with (0.0, 0.0)
+                                    }.sorted(by: { $0.time < $1.time })  // Sort by time
+                                    DispatchQueue.main.async {
+                                        SharedDataManager.shared.hrValues = timeHrWithConfidencePairs.map({($0.time, $0.valueWithConfidence.value)})
+                                        SharedDataManager.shared.hrConfidence = timeHrWithConfidencePairs.map({($0.time, $0.valueWithConfidence.confidence)})
+                                    }
+                                }
+
                                 var strictPulseRate: Double?
                                 if let arrayHrValuesUnwrapped = arrayPulseRatesValues,
                                 arrayHrValuesUnwrapped.count > 0 {
@@ -140,7 +155,7 @@ extension Service.Repository {
                                         SharedDataManager.shared.strictPulseRate = strictPulseRate ?? 0.0
                                     }
                                 }
-                                
+
                                 let arrayBreathRateValues = (breathingRates?.values.map { $0["value"] as? Double }.compactMap { $0 })
 
                                 if let breathingPleth = breath?["rr_trace"] as? [String: [String: Double]] {
@@ -156,7 +171,99 @@ extension Service.Repository {
                                         SharedDataManager.shared.breathingPleth = timeHrPairs
                                     }
                                 }
-                                
+
+                                if let rrWithConfidence = breath?["rr"] as? [String: [String: Double]] {
+                                    // The data might not be in increasing time order so sort it on time
+                                    let timeRrWithConfidencePairs = rrWithConfidence.compactMap { key, value -> (time: Double, valueWithConfidence: (value: Double, confidence: Double))? in
+                                        if let time = Double(key), let value = (value["value"], value["confidence"]) as? (Double, Double) {
+                                            return (time, value)
+                                        }
+                                        return nil  // Skip invalid entries rather than substituting with (0.0, 0.0)
+                                    }.sorted(by: { $0.time < $1.time })  // Sort by time
+                                    DispatchQueue.main.async {
+                                        SharedDataManager.shared.rrValues = timeRrWithConfidencePairs.map({($0.time, $0.valueWithConfidence.value)})
+                                        SharedDataManager.shared.rrConfidence = timeRrWithConfidencePairs.map({($0.time, $0.valueWithConfidence.confidence)})
+                                    }
+                                }
+
+                                if let rrl = breath?["rrl"] as? [String: [String: Double]] {
+                                    // The data might not be in increasing time order so sort it on time
+                                    let timeRrl = rrl.compactMap { key, value -> (time: Double, value: Double)? in
+                                        if let time = Double(key), let value = value["value"] as? Double {
+                                            return (time, value)
+                                        }
+                                        return nil  // Skip invalid entries rather than substituting with (0.0, 0.0)
+                                    }.sorted(by: { $0.time < $1.time })  // Sort by time
+                                    DispatchQueue.main.async {
+                                        SharedDataManager.shared.rrl = timeRrl.map({($0.time, $0.value)})
+                                    }
+                                }
+
+                                if let apnea = breath?["apnea"] as? [String: [String: Double]] {
+                                    // The data might not be in increasing time order so sort it on time
+                                    let timeApnea = apnea.compactMap { key, value -> (time: Double, value: Double)? in
+                                        if let time = Double(key), let value = value["value"] as? Double {
+                                            return (time, value)
+                                        }
+                                        return nil  // Skip invalid entries rather than substituting with (0.0, 0.0)
+                                    }.sorted(by: { $0.time < $1.time })  // Sort by time
+                                    DispatchQueue.main.async {
+                                        SharedDataManager.shared.apnea = timeApnea.map({($0.time, $0.value)})
+                                    }
+                                }
+
+                                if let ie = breath?["ie"] as? [String: [String: Double]] {
+                                    // The data might not be in increasing time order so sort it on time
+                                    let timeIe = ie.compactMap { key, value -> (time: Double, value: Double)? in
+                                        if let time = Double(key), let value = value["value"] as? Double {
+                                            return (time, value)
+                                        }
+                                        return nil  // Skip invalid entries rather than substituting with (0.0, 0.0)
+                                    }.sorted(by: { $0.time < $1.time })  // Sort by time
+                                    DispatchQueue.main.async {
+                                        SharedDataManager.shared.ie = timeIe.map({($0.time, $0.value)})
+                                    }
+                                }
+
+                                if let amplitude = breath?["amplitude"] as? [String: [String: Double]] {
+                                    // The data might not be in increasing time order so sort it on time
+                                    let timeAmplitude = amplitude.compactMap { key, value -> (time: Double, value: Double)? in
+                                        if let time = Double(key), let value = value["value"] as? Double {
+                                            return (time, value)
+                                        }
+                                        return nil  // Skip invalid entries rather than substituting with (0.0, 0.0)
+                                    }.sorted(by: { $0.time < $1.time })  // Sort by time
+                                    DispatchQueue.main.async {
+                                        SharedDataManager.shared.amplitude = timeAmplitude.map({($0.time, $0.value)})
+                                    }
+                                }
+
+                                if let baseline = breath?["baseline"] as? [String: [String: Double]] {
+                                    // The data might not be in increasing time order so sort it on time
+                                    let timeBaseline = baseline.compactMap { key, value -> (time: Double, value: Double)? in
+                                        if let time = Double(key), let value = value["value"] as? Double {
+                                            return (time, value)
+                                        }
+                                        return nil  // Skip invalid entries rather than substituting with (0.0, 0.0)
+                                    }.sorted(by: { $0.time < $1.time })  // Sort by time
+                                    DispatchQueue.main.async {
+                                        SharedDataManager.shared.baseline = timeBaseline.map({($0.time, $0.value)})
+                                    }
+                                }
+
+                                if let phasic = pressure?["phasic"] as? [String: [String: Double]] {
+                                    // The data might not be in increasing time order so sort it on time
+                                    let timePhasic = phasic.compactMap { key, value -> (time: Double, value: Double)? in
+                                        if let time = Double(key), let value = value["value"] as? Double {
+                                            return (time, value)
+                                        }
+                                        return nil  // Skip invalid entries rather than substituting with (0.0, 0.0)
+                                    }.sorted(by: { $0.time < $1.time })  // Sort by time
+                                    DispatchQueue.main.async {
+                                        SharedDataManager.shared.phasic = timePhasic.map({($0.time, $0.value)})
+                                    }
+                                }
+
                                 var strictBreathingRate: Double?
                                 if let arrayRRValuesUnwrapped = arrayBreathRateValues,
                                    arrayRRValuesUnwrapped.count > 0 {
@@ -166,7 +273,7 @@ extension Service.Repository {
                                         SharedDataManager.shared.strictBreathingRate = strictBreathingRate ?? 0.0
                                     }
                                 }
-                                
+
                                 // Print other values
                                 debugPrint("ID: \(id ?? "")")
                                 debugPrint("Upload Date: \(uploadDate ?? "")")
@@ -199,13 +306,13 @@ extension Service.Repository {
             }
 
         }
-        
+
         func completeDataUpload(num: Int, UrlsCount: Int, vid_id: String, upload_id: String, parts: inout [[String : Any]], completion: @escaping (Result<Bool, Error>) -> Void) {
             guard let _ = URL(string: Service.Routes.Base.URL.rawValue),
                   let url = URL(string: Service.Routes.Base.URL.rawValue + Service.Routes.URLs.Complete.rawValue) else {
                 return
             }
-            
+
             do {
                 let _json: [String: Any] = [
                     "id": vid_id,
@@ -229,7 +336,7 @@ extension Service.Repository {
 
         }
 
-        
-        
+
+
     }
 }
